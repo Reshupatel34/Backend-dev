2aafa0ef6b41b39ddd552a83451c4edd6741088c
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-02-10 14:19:54 +0530

    Delete EXPRESS directory

--- EXPRESS/express-project-1/data.js
+++
@@ -1,5 +0,0 @@
-const  user = [
-  { id: 1, name: "reshu", password: "hello123" },
-  { id: 2, name: "riya", password: "hi123" },
-];
-export default user;
--- EXPRESS/express-project/data.js
+++
@@ -1,7 +0,0 @@
-let userDetails=[
-    {id:1,name:"Reshu",age:20,gender:"Female",task:'info',lang:'en'},
-    {id:2,name:"Riya",age:18,gender:"Female"},
-    {id:3,name:"Ravil",age:22,gender:"Male"}
-];
-
-module.exports =userDetails;
