f899caa255cc754e713c4b10f182ad1db5d9c474
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-04-25 00:41:31 +0530

    Added other functionalities like bcrypt and jsonwebtoken to prevent unauthenticated access

--- AuthenticationAuthorizationUsingForm/server.js
+++ AuthenticationAuthorizationUsingForm/server.js
@@ -1,37 +1,76 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cookieParser from "cookie-parser";
 import path from "path";
-import userSchema from './Model/userModel.js';
+import userSchema from "./Model/userModel.js";
 import { fileURLToPath } from "url";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
-const app=express();
-
+const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-app.get('/',(req ,res)=>{
-   res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/create", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const createdUser = await userSchema.create({
+      username,
+      email,
+      password: hash,
+    });
+   const token=jwt.sign({email:createdUser.email},'secret');
+   res.cookie("token",token);
+    res.send(createdUser);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 
-app.post('/create',async(req,res)=>{
-      const {username,email,password}=req.body;
-      const user = await userSchema.create({
-        username,
-        emai,password
-      });
-      
+
+// for rendering login page
+app.get('/login',(req ,res)=>{
+    res.render("login");
+});
+
+app.post('/login',async(req,res)=>{
+     const user=await userSchema.findOne({email:req.body.email});
+      if(!user){
+        return res.send("Email or Password Doesnot exist");
+       }
+
+
+   const result=await bcrypt.compare(req.body.password,user.password);
+   if(!result){
+     return res.send("You cant login");
+   }
+   else{
+      const token=jwt.sign({email:user.email},'secret');
+   res.cookie("token",token);
+    res.send("Logged in")
+   }
+});
+
+// logout
+app.get('/logout',(req,res)=>{
+    res.clearCookie("token");
+    res.redirect('/');
 });
 
 
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
